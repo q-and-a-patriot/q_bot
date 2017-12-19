81a4903f08f7d926bfeb89eb2c1135cd5731ffd8
q-and-a-patriot <>
2017-12-18 20:35:28 -0800

    Everything has changed. This bot is actually running and working.

--- bot.rb
+++ bot.rb
@@ -1,25 +1,72 @@
 require 'twitter'
-$stdout.sync = true
-num = 1
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key = ENV['consumer_key']
-  config.consumer_secret = ENV['consumer_secret']
-  config.access_token = ENV['access_token']
-  config.access_token_secret = ENV['access_token_secret']
+require 'pry'
+
+class QBot
+  attr_reader :map
+
+  def initialize(file)
+    file = File.open(file)
+    @map = JSON.parse file.read
+
+    $stdout.sync = true
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV['consumer_key']
+      config.consumer_secret = ENV['consumer_secret']
+      config.access_token = ENV['access_token']
+      config.access_token_secret = ENV['access_token_secret']
+    end
+
+  end
+
+  def post_thread(arr)
+    first_tweet = arr[0]
+    to_reply = @client.update(first_tweet)
+
+    arr[1..-1].each do | tweet |
+      puts "Printing Tweet #{tweet}"
+      sleep(3)
+
+      to_reply = @client.update(tweet, in_reply_to_status_id: to_reply.id)
+    end
+  end
+
+  def start(post_num=0)
+    loop do
+      post = @map[post_num]
+      post_q_msg post
+      post_num += 1
+      post_num %= 352
+      puts "Posting Q MSG # #{post_num}"
+    end
+  end
+
+  def post_q_msg(q_msg)
+    q_msg.each do |q_line|
+      puts "Posting Q Line"
+      puts q_line
+      post_thread q_line
+      sleep(1600)
+
+    end
+  end
+
+  def get_last_tweet
+    t = @client.user_timeline('Q_AND_A_PATRIOT', count: 1)[0]
+    return t.text
+  end
+
+  def parse_number_from_last_tweet
+    t = get_last_tweet
+    num = t.split(" ")[0].split("_")[-2].to_i - 1
+    num
+  end
+
 end
-t = client.user_timeline('Q_AND_A_PATRIOT', count: 1)[0]
-loop do
-  "Hello! Time for some tweeting!"
-  puts "Tweet Number: "+num.to_s
-  num = num + 1
-  tweet = [*('a'..'z'),*('0'..'9')].shuffle[0,8].join
 
-  iter = t.text[-1, 1]
-  iter = iter.to_i + 1
-
-  t = client.update(tweet+"_"+iter.to_s)
-
-  puts t.text
-  puts "Going to sleep for 600 seconds"
-  sleep(600)
+def random
+ [*('a'..'z'),*('0'..'9')].shuffle[0,8].join
 end
+
+bot = QBot.new("map.json")
+
+bot.start(bot.parse_number_from_last_tweet)
---
+++ format_csv.rb
@@ -0,0 +1,130 @@
+require 'CSV'
+require 'pry'
+require 'json'
+
+file_location = "map.csv"
+
+
+class FormatMap
+
+  def initialize(location)
+    @data = CSV.read(location)
+    @transposed = @data.transpose
+    @msg_start = []
+
+    @transposed[1].each_with_index do |cell, i|
+      if cell.nil?
+        cell = ""
+      end
+      if cell[0,2] == ">>"
+        @msg_start << i
+      end
+    end
+  end
+
+  def full_q_msg(start)
+
+    ceiling = @msg_start[start]
+    if @msg_start[start] == @msg_start.last
+      floor = @data.length
+    else
+      floor = @msg_start[start+1]
+    end
+    length = floor - ceiling
+    output = @data[ceiling, length]
+  end
+
+  def threaded_msg(msg_num)
+    full_msg = full_q_msg msg_num
+    output_array =[]
+
+    full_msg[1..-1].each_with_index do | line, index |
+      # remove extra cells
+      line = line[1,3]
+      output_array << threadify(line, index, msg_num, full_msg)
+    end
+    output_array
+  end
+
+  def threadify(line, index, msg_num, full_msg)
+    tweet_arr = []
+    q_msg_text = line[0]
+    concise_answer = line[1]
+    long_answer = line[2]
+
+    first_line = %(BEGIN_Q_MSG_#{msg_num+1}_#{index+1}
+#{full_msg[0][3]}
+#{full_msg[0][1]}
+)
+    expl_line = %(EXPL_Q_MSG_#{msg_num+1}_#{index+1}
+)
+    further_detail_line = %(DETAIL_Q_MSG_#{msg_num+1}_#{index+1}
+)
+    hashtags ="#Trump #MAGA #Qanon #Q #TheStorm #CBTS #FolllowTheWhiteRabbit #QClearance #TickTock #Breadcrumbs \n\n"
+    if q_msg_text
+      tweet_arr.concat split_and_format_msg(hashtags + q_msg_text, first_line)
+    end
+    if concise_answer
+      tweet_arr.concat split_and_format_msg("[EXPLANATION] " + concise_answer, expl_line)
+    end
+    if long_answer
+      tweet_arr.concat split_and_format_msg("[FURTHER DETAIL] " + long_answer, further_detail_line)
+    end
+    return tweet_arr
+  end
+
+  def split_and_format_msg(msg, prepend_line)
+    out = []
+    chunked_msg = []
+    if msg.length > 260-(prepend_line.length+7)
+      chunked_msg = chunk(msg, 260-(prepend_line.length+5))
+    else
+      chunked_msg << msg
+    end
+    chunked_msg.each_with_index do |chunk, i|
+      full_msg = prepend_line +"\n[#{i+1}/#{chunked_msg.length}] "+chunk
+      out << full_msg
+    end
+
+    return out
+  end
+
+  def chunk(string, size)
+    arr = []
+    until string.length < size
+      last_space_index = string[0, size].rindex(/[\s\r\n\t]/)
+      arr << string[0..last_space_index]
+      string = string[last_space_index+1..-1]
+    end
+    arr << string
+  end
+
+  def format_all
+    output = []
+    @msg_start.each_with_index do | msg_num, index |
+      output << threaded_msg(index)
+    end
+    return output
+
+  end
+end
+
+output = FormatMap.new(file_location)
+
+# out = output.threaded_msg(3)[1]
+# print out
+
+out = output.format_all
+
+json_hash = out.to_json
+
+puts json_hash
+
+file = File.open("map.json", "w")
+
+file.write json_hash
+
+file.close
+
+# TODO
+# Comment everything
